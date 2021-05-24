0b597a530f7e9596995a6fcafd434233446c5d63
GERVY <play_main_1001@outlook.com>
2021-05-24 10:24:56 -0500

    penultimo

--- ProyectProgra2/src/CLASES/clases.java
+++ ProyectProgra2/src/CLASES/clases.java
@@ -14,7 +14,8 @@ import javax.swing.JTextField;
  * @author eltu_
  */
 public class clases {
-    public void soloMayuscula(JTextField txt, JTextField txt2){
+
+    public void soloMayuscula(JTextField txt, JTextField txt2) {
         txt.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent arg0) {
@@ -22,26 +23,75 @@ public class clases {
                 String cad = ("" + c).toUpperCase();
                 c = cad.charAt(0);
                 arg0.setKeyChar(c);
-                
+
                 if (Character.isDigit(c) == false) {
-                }else{
+                } else {
                     arg0.consume();
                 }
-                
+
             }
 
             @Override
             public void keyPressed(KeyEvent arg0) {
-                if(arg0.getKeyCode()==KeyEvent.VK_ENTER){
+                if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
                     txt2.requestFocus();
                 }
             }
 
             @Override
             public void keyReleased(KeyEvent arg0) {
-                
+
             }
         });
     }
-    
+
+    public void mayusculaNumber(JTextField txt) {
+        txt.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent arg0) {
+                char c = arg0.getKeyChar();
+                String cad = ("" + c).toUpperCase();
+                c = cad.charAt(0);
+                arg0.setKeyChar(c);
+
+//                if (Character.isDigit(c) == false) {
+//                }else{
+//                    arg0.consume();
+//                }
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+    }
+    public void soloNumber(JTextField txt) {
+        txt.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent arg0) {
+                char c = arg0.getKeyChar();
+                String cad = ("" + c).toUpperCase();
+                c = cad.charAt(0);
+                arg0.setKeyChar(c);
+
+                if (Character.isDigit(c) == false) {
+                    arg0.consume();
+                }else{
+                    
+                }
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+    }
 }
